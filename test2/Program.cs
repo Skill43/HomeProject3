041a319c99ac07509fe41604c00e3f2205ac0b67
Илья <ilacikoridze357@gmail.com>
2022-12-14 21:55:24 +0300

    Сумма цифр числа

---
+++ test2/Program.cs
@@ -0,0 +1,26 @@
+﻿int inputNumber()
+{
+    int number;
+    string text;
+
+    while (true)
+    {
+        Console.Write("Введите число: ");
+        text = Console.ReadLine();
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+        Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+    }
+    return number;
+}
+int num =inputNumber();
+int res =0;
+while (num >0)
+{
+    res = res + num % 10;
+    num = num / 10;
+
+}
+System.Console.WriteLine(res);
