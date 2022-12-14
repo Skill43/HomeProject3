e78f833eebed5ca873391ee7f4b981407c2867a4
Илья <ilacikoridze357@gmail.com>
2022-12-14 22:35:20 +0300

    A ^ B

---
+++ test3/Program.cs
@@ -0,0 +1,42 @@
+﻿int inputNumber()
+{
+    int number;
+    string text;
+
+    while (true)
+    {
+        Console.Write("Введите число: ");
+        text = Console.ReadLine();
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+        Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+    }
+    return number;
+}
+int inputNumber1()
+{
+    int number;
+    string text;
+
+    while (true)
+    {
+        Console.Write("Введите степень: ");
+        text = Console.ReadLine();
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+        Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+    }
+    return number;
+}
+int num = inputNumber();
+int n = inputNumber1();
+int res = 1;
+for(int i= 0; i<n; i++)
+{
+    res*= num;
+}
+System.Console.WriteLine("{0} ^ {1} = {2}", num, n, res);
