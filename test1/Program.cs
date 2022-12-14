26c09fc59d18e2786a08ebdefa02f02ec4d7520b
Илья <ilacikoridze357@gmail.com>
2022-12-14 14:38:43 +0300

    Массив из 8

---
+++ test1/Program.cs
@@ -0,0 +1,11 @@
+﻿const int Size = 8;
+const int LeftRange = 0;
+const int RightRange = 1;
+
+int[] array = new int[Size];
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(LeftRange, RightRange + 1);
+}
+System.Console.WriteLine(string.Join(" | ", array));
